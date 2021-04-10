60b76778ad004d205a1a4fcdc7420c017ebec238
Sascha Huber <saschahuberzh@gmail.com>
2021-04-10 16:31:39 +0200

    uses ngFor

--- src/app/flat/flat.component.ts
+++ src/app/flat/flat.component.ts
@@ -9,7 +9,13 @@ export class FlatComponent implements OnInit {
 
   constructor() { }
 
+   datas = [ {"quadratMeter": 32, "etage": "1. Etage", "text": "«Ruhige Ferienwohnung am Rand von Sedrun. Ferienwohnung mit Dusche/WC für max. 4 Personen»", "adresse": "7188 Sedrun", "preis": 80},
+   {"quadratMeter": 30, "etage": "3. Etage", "text": "«Ruhige Ferienwohnung am Rand von Sedrun. Ferienwohnung mit Dusche/WC für max. 2 Personen»", "adresse": "7188 Sedrun", "preis": 60},
+   {"quadratMeter": 30, "etage": "3. Etage", "text": "«Ruhige Ferienwohnung am Rand von Sedrun. Ferienwohnung mit Dusche/WC für max. 2 Personen»", "adresse": "7188 Sedrun", "preis": 60}]
+
   ngOnInit(): void {
   }
+
+
 
 }
